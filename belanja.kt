e20c9637590448d4278b296321fff738e375dffb
ZeroGX <73316891+MDickyGX00@users.noreply.github.com>
2024-01-27 11:38:23 +0700

    Update belanja.kt
    Menambah validasi pada line 26 dan menambah sedikit perubahan pada line 28

--- belanja.kt
+++ belanja.kt
@@ -2,7 +2,7 @@
 fun barang(item : String):Int{
     return when (item){
     	"Gamis" -> 150000
-        "Kaos" -> 50000
+        "Kaos" -> 90000
         "Kemeja" -> 700000
         "Celana" -> 120000
         "Kaos Dalam" -> 20000
@@ -13,18 +13,19 @@ fun barang(item : String):Int{
 
 fun diskon(harga: Int, diskon: Double): Int{
     return (harga * diskon).toInt()
-    
 }
+
 fun total(harga: Int, hargaDiskon: Int): Int{
     return (harga - hargaDiskon).toInt()
     
 }
 
-fun main() {
-    val namaBarang = "Gamis"
-    val diskon = 0.05
+fun mamainin() {
+    val namaBarang = "Celana"
+    val diskon = 0.1
+    val diskon1 = 0.05
     val harga = barang(namaBarang)
-    val hargaDiskon= if (harga>100000) diskon(harga,diskon) else 0
+    val hargaDiskon= if (harga>100000) diskon(harga,diskon) else if (harga>80000) diskon(harga,diskon1)  else 0
     val hargaTotal = total(harga, hargaDiskon)
     println("Baju yang anda beli: \nNama barang\t: $namaBarang \nHarga Barang\t: Rp. $harga")
     println("Diskon \t\t: Rp. $hargaDiskon")
@@ -32,3 +33,4 @@ fun main() {
     
 }
 
+//Saya ubah harga kaos menjadi 90000
